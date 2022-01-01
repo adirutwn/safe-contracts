484caa42223aa84d7e213a9f3888fd76e20ef039
adirutwn <adirut.wn@hotmail.com>
2022-01-01 22:25:40 +0700

    chore: use approveHash instead

--- scripts/transfer-bep20.ts
+++ scripts/transfer-bep20.ts
@@ -1,5 +1,5 @@
 import { ethers } from "hardhat";
-import { buildContractCall, buildSafeTransaction, executeTx, safeSignMessage } from "../src/utils/execution";
+import { buildContractCall, buildSafeTransaction, executeTx, safeApproveHash, safeSignMessage } from "../src/utils/execution";
 
 async function main() {
   const signer = (await ethers.getSigners())[0];
@@ -10,13 +10,22 @@ async function main() {
 
   const data = token.interface.encodeFunctionData("transfer", ["0x88888344157c62D161bA7F98f8774bF9115C766b", ethers.utils.parseEther("3.235")])
   const safeTx = buildSafeTransaction({ to: wbnbAddress, data, safeTxGas: 0, nonce: await safe.nonce() })
-  const sig = await safeSignMessage(signer, safe, safeTx)
+  const sig = await safeApproveHash(signer, safe, safeTx, true)
 
   console.log(safeTx)
   console.log(sig)
 
   // Fill second signature then remove comment
-  // await executeTx(safe, safeTx, [sig, { signer: '', data: ''}])
+  // const tx = await executeTx(safe, safeTx, 
+  //   [
+  //     sig, 
+  //     { 
+  //       signer: '0xC96CaAaC2CD4DF2BFAab55f401e1eF5888b02Ebb',
+  //       data: '0xbd556d1cc4a1ed484d44516a2857f2632ae58e2e8afe6b560641f80d38ac9e540e0325d3ffde8ca22f24492a26df058947097d6eb8ecbecc16a12d33ce153e871f'
+  //     }
+  //   ], { gasLimit: 1000000 }
+  // )
+  // console.log(tx)
 }
 
 main()
